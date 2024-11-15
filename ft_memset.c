6a3d38f9454523e1d5d976885d44393181e624dd
Hodaifa Mouis <hmouis@e1r9p8.1337.ma>
2024-11-15 10:36:36 +0100

    rj3at bsbab ft_strtrim

--- ft_memset.c
+++ ft_memset.c
@@ -15,9 +15,7 @@
 void	*ft_memset(void *b, int c, size_t len)
 {
 	size_t			i;
-	unsigned char	*str;
 
-	str = (unsigned char *)b;
 	i = 0;
 	while (i < len)
 	{
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -35,7 +35,7 @@ static char	*ft_superrdup(const char *s1, size_t start, size_t end)
 	arr = (char *)malloc(sizeof(char) * ((end - start) + 1));
 	if (arr == NULL)
 		return (NULL);
-	while (s1[start] != '\0' && start < end)
+	while (start < end)
 	{
 		arr[i] = s1[start];
 		start++;
@@ -48,7 +48,6 @@ static char	*ft_superrdup(const char *s1, size_t start, size_t end)
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	i;
-	size_t	s1_len;
 	size_t	start;
 	size_t	end;
 	char	*trm_str;
@@ -59,8 +58,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (ft_strdup(""));
 	i = 0;
 	start = 0;
-	s1_len = ft_strlen(s1);
-	end = s1_len;
+    end = ft_strlen(s1);
 	while (check_c(s1[start], set) == 1 && start < end)
 		start++;
 	while (check_c(s1[end - 1], set) == 1 && end > start)
